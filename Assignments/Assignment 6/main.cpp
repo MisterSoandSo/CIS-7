3de183c2bb0112c39d519a979eefc7f8f8c4b17a
MisterSoandSo <mistersoandsoplays@gmail.com>
2017-09-13 16:13:30 -0700

    Assignment 6

---
+++ Assignments/Assignment 6/main.cpp
@@ -0,0 +1,55 @@
+// Assignment 6.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+bool integer_check(int &x)
+{
+	int temp = x % 2;
+	if (temp == 1) return true;		//number is odd
+	 return false;					//number is even
+}
+
+int main()
+{
+	string i_input;
+	int i;
+	bool bool_break = true;
+	
+	while (bool_break)	//while loop1
+	{
+		while (true)		//while loop2
+		{
+			cout << "Please enter an integer value('q' to quit): ";
+			getline(cin, i_input);
+			if (i_input.empty() || !isdigit(i_input[0]) )
+			{
+				if(i_input[0] == 'q') bool_break = false;		//break while loop #1
+			}
+			if (!bool_break || isdigit(i_input[0])) break;		//break while loop #2
+			
+		}
+		if (bool_break == false) break;
+		
+		stringstream convertint(i_input);
+		convertint >> i;
+		if (integer_check(i))
+		{
+			cout  << i << " is an odd number." << endl;
+		}
+		else
+		{
+			cout  << i << " is an even number." << endl;
+		}
+		
+	}
+
+    return 0;
+}
+
