a6c6c7d73e2a5c618bf6a5cdbc7e329cb45faeaa
MisterSoandSo <mistersoandsoplays@gmail.com>
2017-12-01 21:15:14 -0800

    Assignment 19

--- Assignments/Assignment 19/Tree.h
+++ Assignments/Assignment 19/Tree.h
@@ -7,78 +7,241 @@ using namespace std;
 
 class Tree
 {
-private:
-	struct TreeNode
-	{
-		string Node_Name;
-		TreeNode *left;
-		TreeNode *right;
-	};
-
-	TreeNode *root;
-
-	void insert(TreeNode *&nodePtr, TreeNode *&newNode) 
-	{
-		if (nodePtr == NULL)
-			nodePtr = newNode;
-		else if (newNode->Node_Name == "")
-			insert(nodePtr->left, newNode);
-		else 
-			insert(nodePtr->right, newNode);
-	}
-	
-	void displayInOrder(TreeNode *nodePtr) const
-	{
-		if (nodePtr)
+	//public:
+		//class TreeNode;
+	private:
+		class TreeNode
 		{
-			displayInOrder(nodePtr->left);
-			cout << nodePtr->Node_Name << endl;
-			displayInOrder(nodePtr->right);
-		}
-	}
-	void displayPreOrder(TreeNode *nodePtr) const
-	{
-		if (nodePtr)
-		{
-			cout << nodePtr->Node_Name << endl;
-			displayPreOrder(nodePtr->left);
-			displayPreOrder(nodePtr->right);
-		}
-	}
-	void displayPostOrder(TreeNode *nodePtr) const
-	{
-		if (nodePtr)
-		{
-			displayPostOrder(nodePtr->left);
-			displayPostOrder(nodePtr->right);
-			cout << nodePtr->Node_Name << endl;
-		}
-	}
+			private:
+				string Node_Name;
+				TreeNode *left;
+				TreeNode *right;
+			public:
+				string passNode_Name()
+				{
+					return Node_Name;
+				}
+				TreeNode *passleft()
+				{
+					return left;
+				}
+				TreeNode *passright()
+				{
+					return right;
+				}
+				void setNode_Name(string input)
+				{
+					Node_Name = input;
+				}
+				void setleft(TreeNode *left_in)
+				{
+					left = left_in;
+				}
+				void setright(TreeNode *right_in)
+				{
+					right = right_in;
+				}
+				void insert(TreeNode *&nodePtr, TreeNode *&newNode, string left_or_right)
+				{
+					if (nodePtr == NULL)
+					{
+						nodePtr = newNode;
+					}
+					if (left_or_right == "left")
+					{
+						insert(nodePtr->left, newNode, "choosen");
+					}
+					if (left_or_right == "right")
+					{
+						insert(nodePtr->right, newNode, "choosen");
+					}
 
-public:
-	Tree()
-	{
-		root = NULL;
-	}
-	
-	void insertNode(string)
-	{
+				}
+				void displayTreeInOrder(TreeNode *nodePtr) const
+				{
+					if (nodePtr)
+					{
+						displayTreeInOrder(nodePtr->left);
+						cout << nodePtr->Node_Name << endl;
+						displayTreeInOrder(nodePtr->right);
+					}
+				}
+				void displayTreePreOrder(TreeNode *nodePtr) const
+				{
+					if (nodePtr)
+					{
+						cout << nodePtr->Node_Name << endl;
+						displayTreePreOrder(nodePtr->left);
+						displayTreePreOrder(nodePtr->right);
+					}
+				}
+				void displayTreePostOrder(TreeNode *nodePtr) const
+				{
+					if (nodePtr)
+					{
+						displayTreePostOrder(nodePtr->left);
+						displayTreePostOrder(nodePtr->right);
+						cout << nodePtr->Node_Name << endl;
+					}
+				}
+		};
+		int TreeSize;
+		TreeNode *root;
 
-	}
-	
-	
-	void displayInOrder() const
-	{
-		displayInOrder(root);
-	}
-	void displayPreOrder() const
-	{
-		displayPreOrder(root);
-	}
-	void displayPostOrder() const
-	{
-		displayPostOrder(root);
-	}
+	public:
+		Tree()      //initializing Root
+		{
+			root->setNode_Name("Tree Root");
+			root->setleft(NULL);
+			root->setright(NULL);
+			TreeSize = 0;
+		}
+
+		~Tree()
+		{
+			delete root;
+		}
+
+		void insertNode(string input, int choice)
+		{
+			TreeNode *newNode;
+			TreeNode *destination;
+			string user_node;
+			bool done = false;
+
+			newNode = new TreeNode;
+			
+			newNode->setNode_Name(input);
+			newNode->setleft(NULL);
+			newNode->setright(NULL);
+			while (!done)
+			{
+				cout << "Which node do you wish to put it after?: ";
+				cin >> user_node;
+				destination = traversal(user_node, root);
+
+				if (choice == 1)
+				{
+					if (destination->passleft() == NULL)
+					{
+						root->insert(destination, newNode, "left");	//assume left
+						done = true;
+					}
+					else
+					{
+						cout << "The left child of Node " << destination->passNode_Name() << "is not empty.\nInvalid action Please use a valid node!" << endl;
+
+					}
+
+				}
+				if (choice == 2)
+				{
+					if (destination->passright() == NULL)
+					{
+						root->insert(destination, newNode, "right");	//assume right
+						done = true;
+					}
+					else
+					{
+						cout << "The right child of Node " << destination->passNode_Name() << "is not empty.\nInvalid action Please use a valid node!" << endl;
+					}
+
+				}
+			}
+
+
+		}
+
+		TreeNode *traversal(const string & name, TreeNode * base) const
+		{
+			TreeNode * temp;
+
+			if (base == NULL)
+			{
+				return NULL;
+			}
+			string check1 = base->passNode_Name();
+			if ( check1 == name)
+			{
+				return base;
+			}
+
+			temp = traversal(name, base->passleft());
+
+			if (temp)
+			{
+				return temp;
+			}
+
+			return traversal(name, base->passright());
+		}
+
+		int traverseForDepth(TreeNode *base, int currentDepth) const
+		{
+			int maxDepth = 0;
+
+			if (base)
+			{
+				maxDepth = currentDepth;
+
+				traverseForDepth(base->passleft(), currentDepth + 1);
+				traverseForDepth(base->passright(), currentDepth + 1);
+			}
+
+			return maxDepth;
+		}
+
+		bool traverseForComplete(TreeNode * base, int index) const
+		{
+			   if (base == NULL)
+			{
+				return true;
+			}
+
+			if (index >= TreeSize)
+			{
+				return false;
+			}
+
+			return (traverseForComplete(base->passleft(), 2 * index + 1) &&
+					traverseForComplete(base->passright(), 2 * index + 2));
+		}
+
+		int treesize()
+		{
+			return TreeSize;
+		}
+
+		int treedepth()
+		{
+			return traverseForDepth(root,0);
+		}
+
+		bool isFull()
+		{
+			return ((1 << (treedepth() + 1)) - 1 == TreeSize);
+		}
+
+		bool isComplete()
+		{
+			return traverseForComplete(root, 0);
+		}
+
+		void displayInOrder() const
+		{
+			root->displayTreeInOrder(root);
+		}
+
+		void displayPreOrder() const
+		{
+			root->displayTreePreOrder(root);
+		}
+
+		void displayPostOrder() const
+		{
+			root->displayTreePostOrder(root);
+		}
+
 };
 
 #endif
--- Assignments/Assignment 19/main.cpp
+++ Assignments/Assignment 19/main.cpp
@@ -1,46 +1,30 @@
 #include "stdafx.h"
+#include <iostream>
 #include "Tree.h"
 
-int menu(int &temp)
-{
-	temp = 0;
-	while (temp == 0 && temp <= 4)		//not 0 but 1-4 then return
-	{
-		cout << "1 * Add a new node" << endl;
-		cout << "2 * Add a left child" << endl;
-		cout << "3 * Add a right child" << endl;
-		cout << "4 * Exit Program" << endl;
-		cout << "Choose from options (1-4): ";
-		cin >> temp;
-	}
-	return temp;
-}
+using namespace std;
 
-void display()
-{
-
-}
+void display(Tree &);
+void menu(Tree &, int &);
+void add_child(Tree &, int);
 
 int main()
 {
-	Tree start();
+	Tree start;
 	int break_int = 0;
 
-	while (break_int != 4)
+	while (break_int != 3)
 	{
-		menu(break_int);
+		menu(start,break_int);
 		switch (break_int)
 		{
 		case 1:
-
+			add_child(start, 1);
 			break;
 		case 2:
-
+			add_child(start, 2);
 			break;
-		case 3:
-
-			break;
-		case 4:	//Exit
+		case 3:	//Exit
 			cout << "Goodbye!" << endl;
 			break;
 		}
@@ -48,3 +32,49 @@ int main()
     return 0;
 }
 
+void display(Tree &temp)
+{
+	cout << "Number of nodes: "<< temp.treesize() << endl;
+	cout << "Depth of tree: " <<  temp.treedepth() << endl;
+	cout << "Full tree: "<< (temp.isFull() ? "Yes" : "No") <<endl;
+	cout << "Complete tree: "<< (temp.isComplete() ? "Yes" : "No") << endl;
+	//3 traversal
+	cout << "DisplayInOrder: ";
+	temp.displayInOrder();
+	cout << endl<< "DisplayPreOrder: ";
+	temp.displayPreOrder();
+	cout << endl<< "DisplayPostOrder: ";
+	temp.displayPostOrder();
+}
+
+void menu(Tree &temp, int &u_num)
+{
+	display(temp);
+	int choice = 0;
+	while (choice == 0 && choice <= 3)		//not 0 but 1-3 then return
+	{
+		cout << "1 * Add a left child" << endl;
+		cout << "2 * Add a right child" << endl;
+		cout << "3 * Exit Program" << endl;
+		cout << "Choose from options (1-3): ";
+		cin >> choice;
+	}
+	u_num = choice;
+}
+
+void add_child(Tree &temp, int choice)
+{
+    string user_input;
+	cout << "What do you wish to name the node: ";
+    cin >> user_input;
+
+	switch (choice)
+	{
+	case 1:
+		temp.insertNode(user_input,choice);		//left
+		break;
+	case 2:
+		temp.insertNode(user_input,choice);		//right
+		break;
+	}
+}
